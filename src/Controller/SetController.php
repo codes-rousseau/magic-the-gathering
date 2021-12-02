7d0ed82da7c190014ac4ef7c6f8ae668a4abf878
Victor <victor.paboeuf@gmail.com>
2021-12-02 01:51:13 +0100

    Add search by card name, templates improvements

--- src/Controller/SetController.php
+++ src/Controller/SetController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\Type\SearchType;
+use App\Repository\CardRepository;
 use App\Repository\SetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +15,36 @@ class SetController extends AbstractController
     /**
      * @Route("/sets/{id<\d+>}", name="set_show")
      */
-    public function show(int $id, SetRepository $setRepository): Response
+    public function show(int $id, SetRepository $setRepository, CardRepository $cardRepository, Request $request): Response
     {
         $set = $setRepository->find($id);
         if (null === $set) {
             throw $this->createNotFoundException("Set $id not found");
         }
 
-        return $this->render('set/show.html.twig', ['set' => $set]);
+        // Création formulaire de recherche
+        $form = $this->createForm(SearchType::class);
+
+        // Gestion des retours du formulaire de recherche
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+            $cards = $cardRepository->findCardsByName($data['name'], $id);
+            if (count($cards) > 0) {
+                $this->addFlash('success', count($cards).' card(s) found');
+                $returnSetId = $id;
+            } else {
+                $this->addFlash('warning', 'Card not found');
+                $cards = $set->getCards();
+                $returnSetId = false;
+            }
+        } else {
+            $cards = $set->getCards();
+            $returnSetId = false;
+        }
+
+        return $this->render('set/show.html.twig', ['cards' => $cards, 'returnSetId' => $returnSetId, 'form' => $form->createView()]);
     }
 
     /**
---
+++ src/Form/Type/SearchType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name');
+    }
+}
