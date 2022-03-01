2e6698b0fb8009ad5633815ed364c04163e9a115
Slecourtois <slecourtois@infostat.fr>
2022-03-01 22:42:26 +0100

    dependances + service curl Scryfall

---
+++ src/Controller/CartesApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\ScryfallService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartesApiController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+
+    private $scryfallService;
+
+    public function __construct(ScryfallService $scryfallService)
+    {
+        $this->scryfallService = $scryfallService;
+    }
+
+    /**
+     * @return Response
+     * @Route("test")
+     */
+    public function test()
+    {
+
+        $res = $this->scryfallService->getAllCards();
+        dump($res);
+        return new Response('<body>toto</body>');
+    }
+}
---
+++ src/Service/ScryfallService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Service;
+
+class ScryfallService
+{
+
+
+    /**
+     * @return false|string
+     */
+    public function getAllCards()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.scryfall.com/cards/search?q=all',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            return json_decode($response);
+        }
+
+    }
+
+
+
+    public function getCollections()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.scryfall.com/sets/',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            return json_decode($response);
+        }
+    }
+
+
+    public function getCardsByCollection($collection)
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.scryfall.com/cards/search?q=set:'.$collection,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            return json_decode($response);
+        }
+    }
+}
