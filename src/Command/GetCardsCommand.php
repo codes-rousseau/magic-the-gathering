6f33f78660585ba97a8b82548f3308869afe312d
Slecourtois <slecourtois@infostat.fr>
2022-03-02 17:38:01 +0100

    choice in cmd

--- src/Command/GetCardsCommand.php
+++ src/Command/GetCardsCommand.php
@@ -7,8 +7,11 @@ use App\Entity\CardCollection;
 use App\Service\ScryfallService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -20,7 +23,7 @@ class GetCardsCommand extends Command
     private $scryfallService;
     private $entityManager;
 
-    public function __construct(string $name = null,ScryfallService $scryfallService, EntityManagerInterface $entityManager)
+    public function __construct(string $name = null, ScryfallService $scryfallService, EntityManagerInterface $entityManager)
     {
         parent::__construct($name);
         $this->scryfallService = $scryfallService;
@@ -30,129 +33,148 @@ class GetCardsCommand extends Command
     protected function configure(): void
     {
         $this->
-            setName('app:get-cards')
-        ;
+        setName('app:get-cards')
+            ->addArgument('arg1', InputArgument::REQUIRED, 'Nom de la collection');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
 
         $filesystem = new Filesystem();
 
-        $collectionCard = $this->scryfallService->getCollections();
-
-        $tabCollection = $collectionCard->data;
+        $collectionCardName = $this->scryfallService->getCollectionsByName($arg1);
 
 
-        //recherche de toutes les collections
-        for ($i=0; $i<count($tabCollection); $i++) {
-
-            $oCollection = new CardCollection();
-            $oCollection->setCode($tabCollection[$i]->code);
-            $date = new \DateTime($tabCollection[$i]->released_at);
-            $oCollection->setPublishedAt($date);
-            $oCollection->setName($tabCollection[$i]->name);
-            $oCollection->setSvg($tabCollection[$i]->icon_svg_uri);
+        $tabCollectionName = $collectionCardName->data;
 
 
-            //recherche de toutes les cartes de chaque collection
-            $cards = $this->scryfallService->getCardsByCollection($tabCollection[$i]->code);
-
-            //Cas où une collection n'a pas de carte
-            if ($cards->object != 'error') {
-
-                $tabCards = $cards->data;
-
-                for ($p=0; $p<count($tabCards); $p++) {
-
-                    $oCard = new Card();
+        if (count($tabCollectionName) > 1) {
 
 
-                    if (isset($tabCards[$p]->card_faces)) {
-
-                        //Type de cartes carte face contenant plusieurs faces
-                        foreach ($tabCards[$p]->card_faces as $faces) {
-
-                            echo "name face = ".$faces->name."\n";
+            $arrayNames = array_column($tabCollectionName, 'name');
+            $arrayCode = array_column($tabCollectionName, 'code');
 
 
-                            if (isset($faces->image_uris)) {
+            $helper = $this->getHelper('question');
 
-                                $oCard->setName($faces->name);
-                                $oCard->setCollection($oCollection);
+            $question = new ChoiceQuestion(
+                'Plusieurs SETS correspondent, merci de saisir le nom du SET',
+                $arrayNames,
+                0
+            );
 
-                                if (isset($faces->artist)) {
-                                    $oCard->setArtist($faces->artist);
-                                }
+            $collectionName = $helper->ask($input, $output, $question);
 
-                                if (isset($faces->colors) && count($faces->colors)>0) {
-                                    $oCard->setColor($faces->colors[0]);
-                                }
+            $index = array_search($collectionName, array_column($tabCollectionName, 'name'));
 
-                                $oCard->setDescription($faces->oracle_text);
+            $collectionCard = $this->scryfallService->getCollectionsByCode($tabCollectionName[$index]->code);
 
-                                $oCard->setImage($faces->image_uris->normal);
-
-
-
-                                $cardName = str_replace(" ", "_", $faces->name);
-
-                                $oCard->setTarget('/cards/'.$cardName.'.jpg');
+            $tabCollection = $collectionCard;
+        } else {
+            $tabCollection = $collectionCardName;
+        }
 
 
-                                //copy
-                                $filesystem->copy($faces->image_uris->normal, 'public/cards/'.$cardName.'.jpg');
+        //Création de la collection
+        $oCollection = new CardCollection();
+        $oCollection->setCode($tabCollection->code);
+        $date = new \DateTime($tabCollection->released_at);
+        $oCollection->setPublishedAt($date);
+        $oCollection->setName($tabCollection->name);
+        $oCollection->setSvg($tabCollection->icon_svg_uri);
 
-                                $oCollection->addCard($oCard);
+
+        //recherche de toutes les cartes de chaque collection
+        $cards = $this->scryfallService->getCardsByCollection($tabCollection->code);
+
+        //Cas où une collection n'a pas de carte
+        if ($cards->object != 'error') {
+
+            $tabCards = $cards->data;
+
+            for ($p = 0; $p < count($tabCards); $p++) {
+
+                $oCard = new Card();
+
+
+                if (isset($tabCards[$p]->card_faces)) {
+
+                    //Type de cartes carte face contenant plusieurs faces
+                    foreach ($tabCards[$p]->card_faces as $faces) {
+
+                        echo "name face = " . $faces->name . "\n";
+
+
+                        if (isset($faces->image_uris)) {
+
+                            $oCard->setName($faces->name);
+                            $oCard->setCollection($oCollection);
+
+                            if (isset($faces->artist)) {
+                                $oCard->setArtist($faces->artist);
                             }
 
-                        }
+                            if (isset($faces->colors) && count($faces->colors) > 0) {
+                                $oCard->setColor($faces->colors[0]);
+                            }
+
+                            $oCard->setDescription($faces->oracle_text);
+
+                            $oCard->setImage($faces->image_uris->normal);
 
 
-                    } else {
+                            $cardName = str_replace(" ", "_", $faces->name);
 
-                        echo "name = ".$tabCards[$p]->name."\n";
+                            $oCard->setTarget('/cards/' . $cardName . '.jpg');
 
-                        if (isset($tabCards[$p]->image_uris)) {
-
-                            $oCard->setName($tabCards[$p]->name);
-                            $oCard->setCollection($oCollection);
-                            $oCard->setArtist($tabCards[$p]->artist);
-                            $oCard->setColor($tabCards[$p]->border_color);
-                            $oCard->setDescription($tabCards[$p]->oracle_text);
-                            $oCard->setImage($tabCards[$p]->image_uris->normal);
-
-                            $cardName = str_replace(" ", "_", $tabCards[$p]->name);
-
-                            $oCard->setTarget('/cards/'.$cardName.'.jpg');
 
                             //copy
-                            $filesystem->copy($tabCards[$p]->image_uris->normal, 'public/cards/'.$cardName.'.jpg');
+                            $filesystem->copy($faces->image_uris->normal, 'public/cards/' . $cardName . '.jpg');
 
                             $oCollection->addCard($oCard);
                         }
 
-
                     }
 
 
+                } else {
 
+                    echo "name = " . $tabCards[$p]->name . "\n";
 
-                    //enregistrement objet card
-                    $this->entityManager->persist($oCard);
-                    $this->entityManager->flush();
+                    if (isset($tabCards[$p]->image_uris)) {
+
+                        $oCard->setName($tabCards[$p]->name);
+                        $oCard->setCollection($oCollection);
+                        $oCard->setArtist($tabCards[$p]->artist);
+                        $oCard->setColor($tabCards[$p]->border_color);
+                        $oCard->setDescription($tabCards[$p]->oracle_text);
+                        $oCard->setImage($tabCards[$p]->image_uris->normal);
+
+                        $cardName = str_replace(" ", "_", $tabCards[$p]->name);
+
+                        $oCard->setTarget('/cards/' . $cardName . '.jpg');
+
+                        //copy
+                        $filesystem->copy($tabCards[$p]->image_uris->normal, 'public/cards/' . $cardName . '.jpg');
+
+                        $oCollection->addCard($oCard);
+                    }
 
                 }
+
+                //enregistrement objet card
+                $this->entityManager->persist($oCard);
+                $this->entityManager->flush();
+
             }
-
-
-            //enregistrement objet collection
-            $this->entityManager->persist($oCollection);
-            $this->entityManager->flush();
-
         }
 
+
+        //enregistrement objet collection
+        $this->entityManager->persist($oCollection);
+        $this->entityManager->flush();
 
         $io->success('Finish.');
 
--- src/Service/ScryfallService.php
+++ src/Service/ScryfallService.php
@@ -69,6 +69,64 @@ class ScryfallService
     }
 
 
+    public function getCollectionsByName($name)
+    {
+        $curl = curl_init();
+echo 'https://api.scryfall.com/sets/?q=name:'.$name."\n";
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.scryfall.com/sets/?q=name:'.$name,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            return json_decode($response);
+        }
+    }
+
+
+    public function getCollectionsByCode($code)
+    {
+        $curl = curl_init();
+        echo 'https://api.scryfall.com/sets/'.$code."\n";
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.scryfall.com/sets/'.$code,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            return json_decode($response);
+        }
+    }
+
+
     public function getCardsByCollection($collection)
     {
 
