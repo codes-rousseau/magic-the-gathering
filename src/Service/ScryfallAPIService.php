163513f738c9ee422646b2248321cac0af62f319
devock <seb.devaucresson@gmail.com>
2022-11-20 10:19:24 +0100

    Add Scryfall API Service

---
+++ src/Service/ScryfallAPIService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ScryfallAPIService
+{
+    private const BASE_API = 'https://api.scryfall.com';
+
+    private HttpClientInterface $client;
+    private EntityManager $em;
+
+    public function __construct( ) {
+        $this->client = HttpClient::create();
+    }
+
+    public function getCollections(): Object {
+
+        $request_collection = $this->client->request(
+            'GET',
+            self::BASE_API . '/sets'
+        );
+
+        if( $request_collection->getStatusCode() != 200 ) {
+            return [];
+        } else {
+            return json_decode($request_collection->getContent());
+        }
+    }
+
+    public function getCards(String $code): Object {
+        $request_cards = $this->client->request(
+            'GET',
+            self::BASE_API . '/cards/search?q=set:' . $code
+        );
+
+        if( $request_cards->getStatusCode() != 200 ) {
+            return [];
+        } else {
+            return json_decode($request_cards->getContent());
+        }
+    }
+
+}
