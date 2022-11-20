55b60dea5ea37aff07d79eaea049ad364e5fd405
devock <seb.devaucresson@gmail.com>
2022-11-20 14:00:48 +0100

    Fix upload file's extension

--- src/Service/UploadService.php
+++ src/Service/UploadService.php
@@ -11,27 +11,26 @@ class UploadService
     private const BASE_UPLOAD_DIR = 'img';
     private const PREFIXE = 'public';
 
-    public function uploadFile($uri, $dir, $name): String {
+    public function uploadFile(String $uri, String $dir , String $name): String {
 
         $this->createDir($dir);
-        
-        //var_dump(self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR  . '\\' . $dir  . '\\' .  $name . '.svg');
-        if(copy( $uri, self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR  . '\\' . $dir  . '\\' .  $name . '.svg')) {
-            return self::BASE_UPLOAD_DIR  . '\\' . $dir  . '\\' . $name . '.svg';
+
+        if(copy( $uri, self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR  . '\\' . $dir  . '\\' .  $name )) {
+            return self::BASE_UPLOAD_DIR  . '\\' . $dir  . '\\' . $name;
         } else {
             return "";
         }
     }
 
-    private function createDir( $dir ) {
+    private function createDir( String $dir ) {
         $fs = new Filesystem();
 
-        if( !$fs->exists(self::PREFIXE . self::BASE_UPLOAD_DIR) ) {
-            $fs->mkdir(self::PREFIXE . self::BASE_UPLOAD_DIR, 0777);
+        if( !$fs->exists(self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR) ) {
+            $fs->mkdir(self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR, 0777);
         }
 
-        if( !$fs->exists(self::PREFIXE . self::BASE_UPLOAD_DIR . '\\' . $dir) ) {
-            $fs->mkdir(self::PREFIXE . self::BASE_UPLOAD_DIR . '\\' . $dir, 0777);
+        if( !$fs->exists(self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR . '\\' . $dir) ) {
+            $fs->mkdir(self::PREFIXE . '\\' . self::BASE_UPLOAD_DIR . '\\' . $dir, 0777);
         }
     }
 }
